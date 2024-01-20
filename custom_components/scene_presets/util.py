f360cc369c9f5766a52dee693c74cf807d775e25
Sören Beye <github@hypfer.de>
2024-01-20 16:43:25 +0100

    fix: Recursively resolve groups that pretend to be lights

--- custom_components/scene_presets/util.py
+++ custom_components/scene_presets/util.py
@@ -1,4 +1,3 @@
-from homeassistant.components.group import expand_entity_ids
 from homeassistant.helpers import entity_registry, device_registry
 
 def ensure_list(data):
@@ -18,23 +17,24 @@ def resolve_targets(hass, entity_ids, device_ids, area_ids):
 
     resolved_entity_ids = []
 
-    for entity_id in entity_ids:
-        if entity_id.startswith("light."):
-            resolved_entity_ids.extend([entity_id])
-        elif entity_id.startswith("group."):
-            resolved_entity_ids.extend(expand_entity_ids(hass, [entity_id]))
-
-    for device_id in device_ids:
-        registry_entries = entity_registry.async_entries_for_device(entity_reg, device_id)
-
-        resolved_entity_ids.extend(entry.entity_id for entry in registry_entries if entry.domain == 'light')
+    # First we have to resolve areas since they may contain lights, groups or groups that pretend to be lights
     for area_id in area_ids:
         device_entries = device_registry.async_entries_for_area(device_reg, area_id)
 
         for device_entry in device_entries:
             registry_entries = entity_registry.async_entries_for_device(entity_reg, device_entry.id)
 
-            resolved_entity_ids.extend(entry.entity_id for entry in registry_entries if entry.domain == 'light')
+            resolved_entity_ids.extend(entry.entity_id for entry in registry_entries if entry.domain == 'light' or entry.domain == 'group')
+
+    # Then we take all entity IDs, try to figure out if they're lights, groups or groups posing as lights and resolve them down to plain lights
+    for entity_id in entity_ids:
+        resolved_entity_ids.extend(resolve_entity_ids(hass, entity_id))
+
+    # Finally, Devices can be resolved last, because they should never provide groups and thus should only ever contain plain light entities
+    for device_id in device_ids:
+        registry_entries = entity_registry.async_entries_for_device(entity_reg, device_id)
+
+        resolved_entity_ids.extend(entry.entity_id for entry in registry_entries if entry.domain == 'light')
 
     # Deduplicate entity_id list
     resolved_entity_ids = list(set(resolved_entity_ids))
@@ -43,3 +43,25 @@ def resolve_targets(hass, entity_ids, device_ids, area_ids):
     light_entity_ids = [entity_id for entity_id in resolved_entity_ids if entity_id.startswith("light.")]
 
     return light_entity_ids
+
+
+def resolve_entity_ids(hass, entity_id, depth=1):
+    resolved_ids = []
+
+    if not entity_id.startswith(("light", "group")):
+        return resolved_ids
+
+    # If you have groups nested 4+ layers deep, you should rethink your life choices
+    if depth >= 3:
+        return resolved_ids
+
+    if entity := hass.states.get(entity_id):
+        nested_entity_ids = entity.attributes.get("entity_id", [])
+
+        if not nested_entity_ids: # It's just a plain old light
+            resolved_ids.append(entity_id)
+        else: # It's a group possibly pretending to be a light
+            for nested_entity_id in nested_entity_ids:
+                resolved_ids.extend(resolve_entity_ids(hass, nested_entity_id, depth + 1))
+
+    return resolved_ids
