0f3623f6c2d7d098bd2c7d89d7e16d6c504a1197
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-02-21 15:02:47 +0100

    Fix resolve group hidden members (#17)
    * Check hide_members option when resolving grouped lights
    
    * remove typing
    
    * Restore formatting
    
    ---------
    
    Co-authored-by: Sören Beye <github@hypfer.de>

--- custom_components/scene_presets/util.py
+++ custom_components/scene_presets/util.py
@@ -46,6 +46,7 @@ def resolve_targets(hass, entity_ids, device_ids, area_ids):
 
 
 def resolve_entity_ids(hass, entity_id, depth=0):
+    entity_reg = entity_registry.async_get(hass)
     resolved_ids = []
 
     if not entity_id.startswith(("light", "group")):
@@ -61,6 +62,15 @@ def resolve_entity_ids(hass, entity_id, depth=0):
         if not nested_entity_ids: # It's just a plain old light
             resolved_ids.append(entity_id)
         else: # It's a group possibly pretending to be a light
+            # We grab the config entry for this entity to determine if we should resolve the underlying entities
+            if entity_reg_entry := entity_reg.async_get(entity_id):
+                if conf_entry := hass.config_entries.async_get_entry(
+                    entity_reg_entry.config_entry_id
+                ):
+                    if conf_entry.options.get("hide_members", False):
+                        # The (group) config entry is configured to hide group members, so we do not resolve the underlying entities
+                        return [entity_id]
+
             for nested_entity_id in nested_entity_ids:
                 resolved_ids.extend(resolve_entity_ids(hass, nested_entity_id, depth + 1))
 
